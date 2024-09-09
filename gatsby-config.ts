b1909865f324960647f5f80b684d6d215558f815
Ivo Gregurec <ivo@toptal.com>
2024-09-09 11:11:03 +0200

    hubspot

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,20 +3,38 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `iverson`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        trackingIds: [], // Leave this empty until you get your tracking ID
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
+    {
+      resolve: "gatsby-source-hubspot-forms",
+      options: {
+        apiKey: process.env.HUBSPOT_API_KEY, // Use environment variable for security
+      },
+    },
+  ],
 };
 
 export default config;
