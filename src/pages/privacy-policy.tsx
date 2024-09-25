6fb34c0a2c4166fd42fd4eb0eade84469a94ee4e
Ivo Gregurec <ivo@toptal.com>
2024-09-25 13:30:11 +0200

    privacy policy page

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,161 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+const PrivacyPolicyPage = () => {
+    return (
+        <div className="legal">
+            <main className="container">
+                <header style={{ textAlign: "center", marginBottom: "2rem" }}>
+                    <h2>NATIONAL CRYPTOCURRENCY ASSOCIATION</h2>
+                    <h1>Privacy Policy for National<br />
+                        Cryptocurrency Association</h1>
+                    <h6>Effective Date: October 1, 2024</h6>
+                </header>
+
+                <section>
+                    <h3>INTRODUCTION</h3>
+                    <p>
+                        The National Cryptocurrency Association (“NCA”) values your privacy and is committed to protecting your personal information. This notice (the “Notice”) outlines how the National Cryptocurrency Association collects and processes your Personal Data when you use this website, become a member, or interact with services we provide. We prioritize your privacy and ensure that no data will be shared without your explicit consent.
+                    </p>
+                    <p>
+                        “Personal Data” is any information that enables us to identify you, directly or indirectly, and includes data that classifies as personal information, personal data, personally identifiable information, or similar terms under applicable data privacy and security laws and regulations. It does not include data excluded or exempted from those laws and regulations, such as aggregated, anonymized, or deidentified data.
+                    </p>
+                    <p>
+                        By becoming a member of the National Cryptocurrency Association and related properties (together the “Site”), you acknowledge that you have read and understood the processes and policies referred to in this Notice.
+                    </p>
+                </section>
+
+                <section>
+                    <h3>WHO WE ARE</h3>
+                    <p>The National Cryptocurrency Association is 501(c)4 social welfare organization registered in Delaware, USA.</p>
+                    <h4>How to contact us</h4>
+                    <p>
+                        If you have any questions or concerns about this Notice, please contact us by sending an email to <a href="mailto:privacy@NatCryptoAssoc.org">privacy@NatCryptoAssoc.org</a>.
+                    </p>
+                </section>
+
+                <section>
+                    <h3>HOW WE COLLECT PERSONAL DATA</h3>
+
+                    <h4>Personal Data that you give us</h4>
+                    <p>We may collect and process the following Personal Data:</p>
+                    <ul>
+                        <li>
+                            <strong>Contact information</strong>, which you provide when corresponding with us by phone, email, or otherwise. This includes information you provide when you participate in discussion boards or other social media functions on our Site and when you report a problem with our Site. The information you give us may include your first name, last name, and email address.
+                        </li>
+                        <li>
+                            <strong>Membership information</strong>, including your first name, last name, state of residence, and any other information related to your membership.
+                        </li>
+                    </ul>
+
+                    <h4>Personal Data we collect from you</h4>
+                    <p>With regard to each of your visits to the Site we will automatically collect the following information:</p>
+                    <ul>
+                        <li>
+                            <strong>Technical information</strong>, including the Internet protocol (IP) address used to connect your computer or device to the Internet, browser type and version, time zone setting, browser plug-in types and versions, operating system and platform.
+                        </li>
+                        <li>
+                            <strong>Information about your visit</strong>, including pages you viewed or searched for, page response times, download errors, length of visits to certain pages, page interaction information (such as scrolling, clicks, and mouse-overs), methods used to browse away from the page, and location information.
+                        </li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h3>HOW WE USE YOUR PERSONAL DATA</h3>
+                    <p>
+                        We will only process your Personal Data, including sharing it with third parties, where:
+                    </p>
+                    <ul>
+                        <li>(1) you have provided your consent which can be withdrawn at any time,</li>
+                        <li>(2) the processing is necessary for the performance of a contract to which you are a party,</li>
+                        <li>(3) we are required by law.</li>
+                    </ul>
+                    <p>
+                        We may use Personal Data that you provide directly to us for the following purposes:
+                    </p>
+                    <ul>
+                        <li>To carry out our obligations arising from your membership, or any other contract entered into between you and us, and to provide you with the information, products and membership services;</li>
+                        <li>To organize events that you have purchased or registered for and to provide you with information and other materials relating to the content of the event, the speakers, sponsors, and other attendees;</li>
+                        <li>To provide our newsletter and other publications;</li>
+                        <li>To respond to your questions and provide related membership services;</li>
+                        <li>To provide you with information about other events, products and services we offer that are similar to those that you have already purchased;</li>
+                        <li>To transfer your information as part of a merger or sale of the business;</li>
+                        <li>To notify you about changes to our membership service; and</li>
+                        <li>To ensure that content from our Site is presented most effectively for you and your computer.</li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h3>HOW WE PROTECT YOUR INFORMATION</h3>
+                    <p>
+                        We are committed to protecting your information. We maintain appropriate administrative, technical, and physical safeguards designed to protect the personal information you provide against accidental, unlawful, or unauthorized destruction, loss, alteration, access, disclosure, or use.
+                    </p>
+                </section>
+
+                <section>
+                    <h3>HOW LONG WE RETAIN YOUR INFORMATION</h3>
+                    <p>
+                        We will retain your personal information on file for as long as necessary for our legitimate business purposes. Please note that in certain cases, legal or regulatory obligations require us to retain specific records for a longer period of time.
+                    </p>
+                </section>
+
+                <section>
+                    <h3>CHILDREN'S INFORMATION</h3>
+                    <p>
+                        Our services are not directed to children under the age of 16. If you learn that a child under the age of 16 has provided us with personal information without consent, please contact us at <a href="mailto:privacy@NatCryptoAssoc.org">privacy@NatCryptoAssoc.org</a>.
+                    </p>
+                </section>
+
+                <section>
+                    <h3>LINKS TO THIRD PARTY SITES AND SERVICES</h3>
+                    <p>
+                        Our Site may contain links to third party websites, applications and services not operated by us. These links are provided as a service and do not imply any endorsement by us of the activities or content of these sites, applications or services nor any association with their operators. The NCA is not responsible for the privacy policies or practices of any third party including websites or services directly linked to our service. We encourage you to review the privacy policies of any third party site that you link from our Service.
+                    </p>
+                </section>
+
+                <section>
+                    <h3>YOUR RIGHTS</h3>
+                    <h4>Correction and removal</h4>
+                    <p>
+                        If any of the information that we have about you is incorrect, or you wish to have information (including Personal Data) removed from our records, you may do so by contacting us at <a href="mailto:privacy@NatCryptoAssoc.org">privacy@NatCryptoAssoc.org</a>.
+                    </p>
+
+                    <h4>Opting Out</h4>
+                    <p>
+                        Additionally, if you prefer not to receive marketing messages from us, please let us know by clicking on the unsubscribe link within any marketing message that you receive or by sending a message to us at <a href="mailto:privacy@NatCryptoAssoc.org">privacy@NatCryptoAssoc.org</a>.
+                    </p>
+
+                    <h4>Request erasure of your Personal Data</h4>
+                    <p>
+                        You may have the right to request that Personal Data held about you is deleted.
+                    </p>
+                    <p>
+                        You can exercise any of these rights by contacting us using the Contact Us section on our Site.
+                    </p>
+
+                    <h4>FOR RESIDENTS OF CALIFORNIA ONLY</h4>
+                    <p>
+                        Section 1798.83 of the California Civil Code requires select businesses to disclose policies relating to the sharing of certain categories of your Personal Data with third parties. If you reside in California and have provided your Personal Data to the NCA, you may request information about our disclosures of certain categories of Personal Data to third parties for direct marketing purposes. Such requests must be submitted to us at <a href="mailto:privacy@NatCryptoAssoc.org">privacy@NatCryptoAssoc.org</a>.
+                    </p>
+                </section>
+
+                <section>
+                    <h3>CHANGES TO THIS NOTICE</h3>
+                    <p>
+                        If we make any material changes to this Notice or the way we use, share or collect Personal Data, we will notify you by revising the “Effective Date” at the top of this Notice, prominently posting an announcement of the changes on our Site, or sending an email to the email address you most recently provided us (unless we do not have such an email address) prior to the new policy taking effect.
+                    </p>
+                    <p>
+                        Any changes we make to this Notice in the future will be posted on this page and, where appropriate, notification sent to you by email. Please check back frequently to see any updates or changes to this Notice.
+                    </p>
+                </section>
+
+
+                <footer style={{ marginTop: "2rem" }}>
+                    <Link to="/">Back to Home</Link>
+                </footer>
+            </main>
+        </div>
+    );
+};
+
+export default PrivacyPolicyPage;
