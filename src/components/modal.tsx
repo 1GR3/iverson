109bae06656d12297314bcbb6c3246c65fbc64ad
Ivo Gregurec <ivo@toptal.com>
2024-09-27 14:50:16 +0200

    validation copy

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 
+const AlertIcon = () => {
+    <svg data-testid="geist-icon" height="16" stroke-linejoin="round" viewBox="0 0 16 16" width="16" style="width: 16px; height: 16px; color: var(--ds-red-900);"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.30761 1.5L1.5 5.30761L1.5 10.6924L5.30761 14.5H10.6924L14.5 10.6924V5.30761L10.6924 1.5H5.30761ZM5.10051 0C4.83529 0 4.58094 0.105357 4.3934 0.292893L0.292893 4.3934C0.105357 4.58094 0 4.83529 0 5.10051V10.8995C0 11.1647 0.105357 11.4191 0.292894 11.6066L4.3934 15.7071C4.58094 15.8946 4.83529 16 5.10051 16H10.8995C11.1647 16 11.4191 15.8946 11.6066 15.7071L15.7071 11.6066C15.8946 11.4191 16 11.1647 16 10.8995V5.10051C16 4.83529 15.8946 4.58093 15.7071 4.3934L11.6066 0.292893C11.4191 0.105357 11.1647 0 10.8995 0H5.10051ZM8.75 3.75V4.5V8L8.75 8.75H7.25V8V4.5V3.75H8.75ZM8 12C8.55229 12 9 11.5523 9 11C9 10.4477 8.55229 10 8 10C7.44772 10 7 10.4477 7 11C7 11.5523 7.44772 12 8 12Z" fill="currentColor"></path></svg>
+}
+
 const SignUpModal: React.FC = () => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
@@ -18,7 +22,7 @@ const SignUpModal: React.FC = () => {
         if (firstName.trim() === '') {
             return 'First name is required';
         } else if (!/^[a-zA-Z]{2,30}$/.test(firstName)) {
-            return 'First name must be between 2 and 30 characters and contain only letters';
+            return '<AlertIcon /> This is a mandatory field.';
         }
         return '';
     };
@@ -27,7 +31,7 @@ const SignUpModal: React.FC = () => {
         if (lastName.trim() === '') {
             return 'Last name is required';
         } else if (!/^[a-zA-Z]{2,30}$/.test(lastName)) {
-            return 'Last name must be between 2 and 30 characters and contain only letters';
+            return '<AlertIcon /> This is a mandatory field.';
         }
         return '';
     };
@@ -36,14 +40,14 @@ const SignUpModal: React.FC = () => {
         if (email.trim() === '') {
             return 'Email is required';
         } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-            return 'Email is invalid';
+            return '<AlertIcon /> Please enter a valid email address.';
         }
         return '';
     };
 
     const validateTerms = () => {
         if (!acceptedTerms) {
-            return 'You must accept the Terms and Privacy Policy';
+            return '<AlertIcon /> You must approve the Terms and Privacy Policy.';
         }
         return '';
     };
